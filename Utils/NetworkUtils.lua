ff55ba1f0798cbaabb079b7e5895cc7da4768c64
MoXiaoDi <1753520469@qq.com>
2019-10-24 04:05:34 +0800

    Update and rename NetworkUtils.lua to Utils/NetworkUtils.lua

--- Utils/NetworkUtils.lua
+++ Utils/NetworkUtils.lua
@@ -1,3 +1,28 @@
+LICENSE = [[
+FileName: NetworkUtils.lua
+Author: SmallDi
+Version: 1.0.0
+Date: 2019-10-24
+Email: 1753520469@qq.com
+Github: https://github.com/smalldi/LuaJavaUtils/Utils/NetworkUtils.lua
+Description: 纯LuaJava代码编写的高度自定义对话框
+
+Copyright (C) 2019. SmallDi. All Rights Reserved.
+
+Licensed under the GNU GENERAL PUBLIC LICENSE Version 3, (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.gnu.org/licenses/gpl-3.0.txt
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]
+
+
 local NetworkUtils = {}
 
 --获取网络状态
